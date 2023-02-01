f7757730f3e56ca2258b0067b533f1c82a4e2eab
Tejas20002 <tejashirani55@gmail.com>
2023-02-01 17:52:03 +0530

    [*] terminal site or terminal portfolio

---
+++ js/commands.js
@@ -0,0 +1,65 @@
+var gitlab = "https://www.youtubehttps://gitlab.com/tjhirani";
+var twitter = "https://www.twitter.com/tjhirani/";
+var password = "tjhirani";
+var linkedin = "https://www.linkedin.com/in/tjhirani";
+var instagram = "https://www.instagram.com/tjhirani/";
+var github = "https://github.com/tejas20002/";
+var email = 'mailto:tejashirani55@gmail.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Tejas Hirani!👋",
+  "I'm a Laravel Developer!!!",
+  "After graduating with a Bachelor's in Computer Eng., I worked professionally",
+  "as a software engineer building enterprise web applications for companies.",
+  "the next generation of developers and help them navigate the crazy world that is software", 
+  "development & computer eng..",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'gitlab         <a href="' + gitlab + '" target="_blank">gitlab/tjhirani' + "</a>",
+  'twitter        <a href="' + twitter + '" target="_blank">twitter/tjhirani' + '</a>',
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/tjhirani' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/tjhirani' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/tejas20002' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is TJHirani',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">TJHirani (TJ) Not A Corporation. All TJ reserved.</span>',
+  '██████╗  ██████╗ ██████╗ ███████╗██╗   ██╗███████╗',
+  '██╔══██╗██╔═══██╗██╔══██╗██╔════╝╚██╗ ██╔╝██╔════╝',
+  '██████╔╝██║   ██║██████╔╝█████╗   ╚████╔╝ █████╗  ',
+  '██╔═══╝ ██║   ██║██╔═══╝ ██╔══╝    ╚██╔╝  ██╔══╝  ',
+  '██║     ╚██████╔╝██║     ███████╗   ██║   ███████╗',
+  '╚═╝      ╚═════╝ ╚═╝     ╚══════╝   ╚═╝   ╚══════╝',
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
